9045969e249c5d6cc18cf06144266c93655bede6
Michał Gawron <michal@gawron.name>
2014-11-26 22:43:46 +0100

    Rename files to libusbcc.*

--- libusbcc.h
+++ libusbcc.h
@@ -11,8 +11,8 @@
  * Visit http://www.gnu.org/licenses/gpl-3.0.html for more information on licensing.
  */
 
-#ifndef MULABS_ORG__LIBUSBCC__LIBUSB_H__INCLUDED
-#define MULABS_ORG__LIBUSBCC__LIBUSB_H__INCLUDED
+#ifndef MULABS_ORG__LIBUSBCC__LIBUSBCC_H__INCLUDED
+#define MULABS_ORG__LIBUSBCC__LIBUSBCC_H__INCLUDED
 
 // Standard:
 #include <cstddef>
